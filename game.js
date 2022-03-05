337adec3977a33932668c7a4f7cc63d6da404d42
feie9454 <feie9454@gmail.com>
2022-03-06 00:04:49 +0800

    Add move and damage effect.

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 "use strict"
 const main = document.querySelector("#main")
+const damageRate = 1.11
+let gameSpeed = 10
 let rolesList = []
+let effectlines = []
+const ctx = document.getElementById("main").getContext("2d")
 window.onload = () => {
     if (window.innerHeight / window.innerWidth >= 0.75) {
         main.style.width = "100vw"
@@ -12,6 +16,30 @@ window.onload = () => {
     }
 }
 window.onresize = window.onload
+
+class eline {
+    constructor(posX, posY, eposX, eposY) {
+        this.posX = posX
+        this.posY = posY
+        this.eposX = eposX
+        this.eposY = eposY
+        this.time = gameSpeed
+        effectlines.push(this)
+    }
+    nextTurn() {
+        ctx.strokeStyle="#FFFF00"
+        ctx.lineWidth=15
+        ctx.beginPath()
+        ctx.moveTo(this.posX, this.posY);
+        ctx.lineTo(this.eposX, this.eposY);
+        ctx.closePath();
+        ctx.stroke();
+        this.time--
+        if (this.time == 0) {
+            delete effectlines[effectlines.indexOf(this)]
+        }
+    }
+}
 
 class roles {
     constructor(posX, posY, face, faction) {
@@ -30,82 +58,202 @@ class roles {
         this.mp += this.mpAdd
         this.hp += this.hpAdd
         this.turn++
-        if (this.turn >= this.attspend) {
-            console.log(`${this.name} attacked`);
-            for (let index = 0; index < rolesList.length; index++) {
-                let isSussess = false
-                const element = rolesList[index];
-                if (this.faction == element.faction) { continue }
-                switch (this.face) {
-                    case "right":
-                        if (element.posX - this.posX <= this.sightX && element.posX - this.posX >= -50 && Math.abs(element.posY - this.posY <= this.sightY)) {
+        let isAttacking = false
+        for (let index = 0; index < rolesList.length; index++) {
+            const element = rolesList[index];
+            if (!element) { continue }
+            let isSussess = false
+            if (this.faction == element.faction) { continue }
+            switch (this.face) {
+                case "right":
+                    if (element.posX - this.posX <= this.sightX && element.posX - this.posX >= -50 && Math.abs(element.posY - this.posY) <= this.sightY) {
+                        isAttacking = true
+                        if (this.turn >= this.attspend) {
                             this.attack(element)
                             isSussess = true
                         }
-                        break
-                    case "left":
-                        if (this.posX - element.posX <= this.sightX && this.posX - element.posX >= -50 && Math.abs(element.posY - this.posY <= this.sightY)) {
-                            this.attack(element)
-                            isSussess = true
-                        }
-                        break
-                    case "upon":
-                        if (element.posY - this.posY <= this.sightX && element.posY - this.posY >= -50 && Math.abs(element.posX - this.posX <= this.sightY)) {
-                            this.attack(element)
-                            isSussess = true
-                        }
-                        break
-                    case "bottom":
-                        if (this.posY - element.posY <= this.sightX && this.posY - element.posY >= -50 && Math.abs(element.posX - this.posX <= this.sightY)) {
-                            this.attack(element)
-                            isSussess = true
-                        }
-                        break
-                }
-                if (isSussess) {
-                    this.turn = 0
+                    }
                     break
-                }
+                case "left":
+                    if (this.posX - element.posX <= this.sightX && this.posX - element.posX >= -50 && Math.abs(element.posY - this.posY) <= this.sightY) {
+                        isAttacking = true
+                        if (this.turn >= this.attspend) {
+                            this.attack(element)
+                            isSussess = true
+                        }
+                    }
+                    break
+                case "bottom":
+                    if (element.posY - this.posY <= this.sightX && element.posY - this.posY >= -50 && Math.abs(element.posX - this.posX) <= this.sightY) {
+                        isAttacking = true
+                        if (this.turn >= this.attspend) {
+                            this.attack(element)
+                            isSussess = true
+                        }
+                    }
+                    break
+                case "upon":
+                    if (this.posY - element.posY <= this.sightX && this.posY - element.posY >= -50 && Math.abs(element.posX - this.posX) <= this.sightY) {
+                        isAttacking = true
+                        if (this.turn >= this.attspend) {
+                            this.attack(element)
+                            isSussess = true
+                        }
+                    }
+                    break
             }
+            if (isSussess) {
+                this.turn = 0
+                break
+            }
+        } if (!isAttacking) {
+            this.move()
         }
+
+
     }
     attack(obj) {
         let damage
-        if (this.att > obj.def + 100) {
-            damage = this.att - obj.def
-        } else {
-            damage = getDamage(this.att, obj.def)
+        let realdef = obj.pdef * (1 - ((obj.hpm - obj.hp) / obj.hpm) * (1 / obj.determination))
+        let realatt = this.att * (1 - ((this.hpm - this.hp) / this.hpm) * (1 / this.determination))
+        let realdatt = obj.att * (1 - ((obj.hpm - obj.hp) / obj.hpm) * (1 / obj.determination))
+        let realmdef = obj.mdef * (1 - ((obj.hpm - obj.hp) / obj.hpm) * (1 / obj.determination))
+        switch (this.attype) {
+            case "physic":
+                damage = damageRate ** (realatt - realdef)
+                break
+            case "real":
+                damage = damageRate ** (realatt - Math.min(realdatt, realdef))
+                break
+            case "magic":
+                damage = damageRate ** (realatt - Math.min(realdatt, realdef) + realmdef)
+                break
         }
-        console.log(`${this.name} attacked ${obj.name}, case ${damage} damage`);
-        obj.hp -= damage
-    }
+        damage = Math.ceil(damage)
+        console.log(`${this.name} attacked ${obj.name}, case ${damage} damage by ${this.attype}`);
+        ctx.fillStyle = "#FFFF00";
 
+        new eline(this.posX, this.posY, obj.posX, obj.posY)
+        obj.hp -= damage
+
+
+    }
+    move() {
+        switch (this.face) {
+            case "right":
+                this.posX += this.movespeed
+                break
+            case "left":
+                this.posX -= this.movespeed
+                break
+            case "upon":
+                this.posY -= this.movespeed
+                break
+            case "bottom":
+                this.posY += this.movespeed
+                break
+        }
+
+    }
 }
 class sniper extends roles {
     constructor(posX, posY, face, faction) {
         super(posX, posY, face, faction)
-        this.att = 200
-        this.def = 50
+        this.att = 100
+        this.attype = "physic"
+        this.pdef = 45
+        this.mdef = 0
         this.hpm = 1000
         this.hp = 1000
         this.mp = 0
-        this.attspend = 2
+        this.attspend = 20
+        this.movespeed = 0
         this.sightX = 350
         this.sightY = 150
+        this.determination = 10
     }
 }
 
+class caster extends roles {
+    constructor(posX, posY, face, faction) {
+        super(posX, posY, face, faction)
+        this.att = 100
+        this.attype = "magic"
+        this.pdef = 35
+        this.mdef = 20
+        this.hpm = 1000
+        this.hp = 1000
+        this.mp = 0
+        this.attspend = 30
+        this.movespeed = 0
+        this.sightX = 350
+        this.sightY = 150
+        this.determination = 8
+    }
+}
+
+class tank extends roles {
+    constructor(posX, posY, face, faction) {
+        super(posX, posY, face, faction)
+        this.att = 80
+        this.attype = "physic"
+        this.pdef = 60
+        this.mdef = 8
+        this.hpm = 1000
+        this.hp = 1000
+        this.mp = 0
+        this.attspend = 25
+        this.movespeed = 0
+        this.sightX = 50
+        this.sightY = 50
+        this.determination = 20
+    }
+}
+
+class tankmoving extends roles {
+    constructor(posX, posY, face, faction) {
+        super(posX, posY, face, faction)
+        this.att = 80
+        this.attype = "physic"
+        this.pdef = 60
+        this.mdef = 8
+        this.hpm = 800
+        this.hp = 800
+        this.mp = 0
+        this.attspend = 25
+        this.movespeed = 10
+        this.sightX = 50
+        this.sightY = 50
+        this.determination = 20
+    }
+}
+
+
 function nextTurn() {
     rolesList.forEach(element => {
-        element.nextTurn()
+        if (element) {
+            if (element.hp < 0) {
+                delete rolesList[rolesList.indexOf(element)]
+            } else {
+                element.nextTurn()
+            }
+        }
     });
+    ctx.clearRect(0, 0, 1600, 1200);
+
+    rolesList.forEach(element => {
+        if (element) {
+            ctx.fillStyle = "#FF0000";
+            ctx.fillRect(element.posX - 20, element.posY - 20, 40, 40);
+        }
+    });
+        effectlines.forEach(element => element.nextTurn())
+
 }
-function getDamage(att, def) {
-    let n = 10 ** (1 / 50)
-    let damage = Math.ceil(n ** (att - def))
-    if (damage >= 100) {
-        return 100
-    } else {
-        return damage
-    }
+
+function Ticking() {
+    nextTurn()
+    setTimeout(() => {
+        Ticking()
+    }, 1 / gameSpeed * 1000);
 }
